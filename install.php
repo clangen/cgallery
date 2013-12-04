f4160d15551c46458904797f5d8cfe7f0741b18d
clangen <casey.langen@gmail.com>
2013-12-04 00:45:33 -0800

    build.php -> install.php, with updated output

--- install.php
+++ install.php
@@ -1,26 +1,41 @@
 <?php
   /*
-   * this is a simple, command-line php file that can be used to generate
-   * albums and series based on directory structure. this is especially
-   * useful if you're serving lots of traffic and don't want php to scan
-   * the filesystem for updated images on each request. the basic logic
-   * is as follows:
+   * this is a simple, command-line php file that can be used to generate a gallery of
+   * albums and series based on directory structure.
    *
    * arguments:
-   *   -s /path/to/cgallery/src  : where album.php and series.php live. default=cwd
-   *   -p /path/to/images/dir      : location of root directory that should be indexed. required.
-   *   -d true|false         : if true, thumbnails will be deleted, then re-created. optional.
+   *   -s /path/to/cgallery/src : where album.php and series.php live. default=cwd
+   *   -p /path/to/images/dir   : location of root directory that should be indexed. required.
+   *   -d true|false            : if true, thumbnails will be deleted, then re-created. optional.
    *   -m static|dynamic|local  : static means generic static html, dynamic will symlink php files
    *   -t series|album          : root type for this run
    */
-
   function err($msg, $code = 999) {
-    print "\n  * $msg *\n\naborted.\n\n";
+    if ($code == "quiet") {
+      print "\naborted.\n\n";
+      exit(998);
+    }
+
+    print "\n";
+
+    print "ERROR: $msg\n\n";
+
+    print "required:\n";
+    print "  -p /path/to/images/dir   : gallery files will be copied here.\n\n";
+
+    print "optional:\n";
+    print "  -m static|dynamic|local  : generation mode. default=dynamic\n";
+    print "  -t series|album          : page type. default=series\n";
+    print "  -d true|false            : delete thumbnails. default=false\n";
+    print "  -s /path/to/cgallery/src : default=`cwd`\n\n";
+
+    print "aborted.\n\n";
+
     exit($code);
   }
 
   function fin() {
-    print("\nfinished\n\n");
+    print("successful\n\n");
   }
 
   /* ugh, http://stackoverflow.com/questions/1091107/how-to-join-filesystem-path-strings-in-php.
@@ -51,9 +66,9 @@
     }
   }
 
-  /* compile the specified directory as an album. will use album.php
+  /* install the specified directory as an album. will use album.php
   to generate a static index.html file */
-  function compile_album($dir) {
+  function install_album($dir) {
     $html = path($dir, "index.html");
     $php = path($dir, "index.php");
     $thumbs = path($dir, ".thumbs");
@@ -79,24 +94,23 @@
 
     if ($mode == "static" || $mode == "local") {
       print "  generating thumbnails to $thumbs using $php\n";
-      print "    note: this will silently create missing thumbnails\n";
+      print "  note: this will silently create missing thumbnails\n";
       exec("php $album -m $mode > index.html");
-      print "  created $mode index.html file\n";
+      print "  created $mode index.html file\n\n";
     }
     else if ($mode == "dynamic") {
       exec("php $album");
       print "  linking $album to $php\n";
-      print "    note: no thumbnails will be generated until the next page visit'\n";
+      print "  note: no thumbnails will be generated until the next page visit'\n\n";
       symlink($album, $php);
     }
 
     chdir($oldcwd);
-    print "  done.\n\n";
   }
 
-  /* compile the specified directory as a series. will use series.php
+  /* install the specified directory as a series. will use series.php
   to generate a static index.html file */
-  function compile_series($dir) {
+  function install_series($dir) {
     $html = path($dir, "index.html");
     $php = path($dir, "index.php");
     global $series;
@@ -114,39 +128,37 @@
       will implicitly trigger a thumbnail refresh */
       print "  generating $mode index.html using series.php\n";
       exec("php $series -m $mode > index.html"); /* ugh, but it works and is easy */
-      print "  done\n\n";
     }
     else if ($mode == "dynamic") {
       print "  linking $series to $php\n";
       symlink($series, $php);
-      print "  done\n\n";
     }
 
-    /* compile each sub-directory. compile will figure out if the
+    /* install each sub-directory. install will figure out if the
     specified directory is an album or a nested series */
     foreach (glob("*", GLOB_ONLYDIR) as $current) {
-      compile(path($dir, $current));
+      install(path($dir, $current));
     }
 
     chdir($oldcwd);
   }
 
-  /* given a directory, compiles it as an album or series */
-  function compile($dir, $override = null) {
+  /* given a directory, installs it as an album or series */
+  function install($dir, $override = null) {
     $type = file_exists(path($dir, ".series")) ? "series" : "album";
 
-    print "compiling: " . $dir . "\n\n";
-    print "  type: " . $type . "\n";
+    print "type: " . $type . "\n";
+    print "  path: " . $dir . "\n";
 
     /* use override if specified, otherwise we'll
     default to album. */
     $type = $override ?: $type;
 
     if ($type == "series") {
-      compile_series($dir);
+      install_series($dir);
     }
     else {
-      compile_album($dir);
+      install_album($dir);
     }
   }
 
@@ -162,48 +174,56 @@
 
   $validModes = array("dynamic", "static", "local");
   if (!in_array($mode, $validModes)) {
-    err("ERROR: --mode $mode is not valid. please use dynamic, static, or local");
+    err("'-m $mode' is not valid. please use " . implode('|', $validModes));
   }
 
-  /* show configuration to the user */
-  print "\nbuild will use the following config:\n\n";
-  print "  src dir: " . $src . "\n";
-  print "  dst dir: " . $options["p"] . "\n";
-  print "  re-generate thumbnails? " . ($rethumb ? "yes" : "no") . "\n";
-  print "  type: " . $type . "\n";
-  print "  mode: " . $mode . "\n";
+  $validTypes = array("album", "series");
+  if (!in_array($type, $validTypes)) {
+    err("'-t $type' is not valid. please use " . implode('|', $validTypes));
+  }
 
   if (!$options["p"]) {
-    err("ERROR: -p is required, please specify a target directory.");
+    err("-p is required, please specify a target directory.");
   }
-
-  print "\nthis will overwrite any existing index.html and index.php files\n";
-
-  print "\nconfirm? y/n ";
-
-  /* give the user a chance to back out */
-  $stdin = fopen('php://stdin', 'r');
-  $confirm = strip(fgets($stdin));
-
-  if ($confirm != "y") { /* must be exactly 'y' */
-    err("user canceled", 1);
-  }
-
-  print("\n");
 
   /* check env */
   $album = resolve(path($src, "album.php"));
   $series = resolve(path($src, "series.php"));
 
   if (!file_exists($album) || !file_exists($series)) { /* src dir invalid */
-    err("ERROR: album.php or series.php not found in $src", 2);
+    err("album.php or series.php not found in $src", 2);
   }
 
   if (!is_dir($dst)) { /* target dir invalid */
-    err("ERROR: target path $dst does not appear to be a directory", 3);
+    err("target path $dst does not appear to be a directory", 3);
   }
 
+  /* show configuration to the user */
+  print "\n";
+  print "gallery will be installed as follows:\n\n";
+  print "  install dir   : " . $options["p"] . "\n";
+  print "  mode          : " . $mode . "\n";
+  print "  type          : " . $type . "\n";
+  print "  delete thumbs : " . ($rethumb ? "true" : "false") . "\n";
+  print "  source dir    : " . $src . "\n\n";
+
+  $warndir = $options["p"];
+
+  print "warning: this will recursively delete/replace all index.html and index.php files inside '$warndir'\n\n";
+
+  print "confirm? y/n ";
+
+  /* give the user a chance to back out */
+  $stdin = fopen('php://stdin', 'r');
+  $confirm = strip(fgets($stdin));
+
+  if ($confirm != "y") { /* must be exactly 'y' */
+    err("user canceled", "quiet");
+  }
+
+  print("\n");
+
   /* kick the whole thing off! */
-  compile($dst, $type);
+  install($dst, $type);
   fin();
 ?>
